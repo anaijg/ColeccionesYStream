48f5851ad96c5677b7bbabbebe1dea411d571d04
DanteSlay <danteslay16@gmail.com>
2023-04-21 12:14:23 +0200

    Ejercicios 4 y 5 con lambda

---
+++ src/ejercicio05/NumerosLambda.java
@@ -0,0 +1,45 @@
+package ejercicio05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NumerosLambda {
+    //ATRIBUTOS
+    private List<Integer> numerosUsuario;
+
+    //CONSTRUCTOR
+    public NumerosLambda() {
+        numerosUsuario = new ArrayList<>();
+        pedirNumeros();
+
+    }
+
+    //GETTER
+    public List<Integer> getNumerosUsuario() {
+        return numerosUsuario;
+    }
+
+
+    //FUNCIONES
+    private void pedirNumeros() {
+        String texto = "Introduce números reales, 0 para salir: ";
+        while (true) {
+            System.out.println(texto);
+            int numero = in.nextInt();
+            in.nextLine();
+
+            if (numero == 0) {
+                break;
+            } else if (numero != 0) {
+                numerosUsuario.add(numero);
+            } else{
+                texto = "Eso no es un número real, vuelve a intentarlo.";
+            }
+        }
+    }
+
+
+
+    static Scanner in = new Scanner(System.in);
+}
